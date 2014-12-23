68562681915089ceaca1ed04bcbf6732ebab96be
Tom Hutchinson <git@tomisme.com>
2014-12-23 19:39:17 +0800

    added Template.jsx

---
+++ Template.jsx
@@ -0,0 +1,15 @@
+"use strict"
+
+var React = require("react");
+
+var REPLACEME = React.createClass({
+  render: function() {
+    return (
+      <div>
+        Text Goes Here
+      </div>
+    );
+  }
+});
+
+module.exports = REPLACEME;
